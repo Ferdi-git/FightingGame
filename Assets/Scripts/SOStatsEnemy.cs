d6bb6c956bd4060bfd1b071210a28aefcfb2549a
Ferdinand MOURETTE <f_mourette@stu-bellecour.fr>
2026-02-09 16:05:44 +0100

    r

---
+++ Assets/Scripts/SOStatsEnemy.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class SOStatsEnemy : ScriptableObject 
+{
+    public float maxHealth = 100;
+    public float defense = 0f;
+    public float strength = 20f;
+    public float healMult = 0.2f;
+    public float critChance = 0.2f;
+    public float critMult = 2f;
+
+}
